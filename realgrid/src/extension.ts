9d4d0e1a48b910b01752554e88269596319a3f18
Ryu <ryujt658@gmail.com>
2021-07-20 22:37:23 +0900

    Code snippet, Completion Extension 예제 코드 (RealGrid를 예제로 사용)

---
+++ realgrid/src/extension.ts
@@ -0,0 +1,49 @@
+import * as vscode from 'vscode';
+import * as bs from './basic-codes';
+
+export function activate(context: vscode.ExtensionContext) {
+
+	const ruleBasicCode = {
+		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+			const realGridCompletion = new vscode.CompletionItem('RealGrid');
+
+			const snippetCompletion = new vscode.CompletionItem('RG Basic Code');
+			snippetCompletion.insertText = new vscode.SnippetString(bs.basicCode);
+			snippetCompletion.documentation = new vscode.MarkdownString("Inserts a basic codes for RealGrid.");
+
+			return [
+				realGridCompletion,
+				snippetCompletion,
+			];
+		}		
+	};
+	const providerBasicCode = vscode.languages.registerCompletionItemProvider(
+		['html', 'javascript'], 
+		ruleBasicCode
+	);
+
+	const ruleCompletion = {
+		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+			const linePrefix = document.lineAt(position).text.substr(0, position.character);
+			if (!linePrefix.endsWith('RealGrid.')) {
+				return undefined;
+			}
+
+			return [
+				new vscode.CompletionItem('LocalDataProvider', vscode.CompletionItemKind.Method),
+				new vscode.CompletionItem('LocalTreeDataProvider', vscode.CompletionItemKind.Method),
+				new vscode.CompletionItem('GridView', vscode.CompletionItemKind.Method),
+				new vscode.CompletionItem('TreeView', vscode.CompletionItemKind.Method),				
+			];
+		}
+	}; 
+	const providerCompletion = vscode.languages.registerCompletionItemProvider(
+		['html', 'javascript'], 
+		ruleCompletion, '.'
+	);
+
+	context.subscriptions.push(
+		providerBasicCode, 
+		providerCompletion
+	);
+}
