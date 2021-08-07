d42e0ec069011381e2d64c563e127ccf7915a029
Ryu <ryujt658@gmail.com>
2021-08-08 03:34:24 +0900

    리얼그리드 현재 녹화하는 예제에 맞춰서 테스트로 만들어 봄.
    아직 파싱 처리하기 전
    리얼그리드 녹화는 https://www.youtube.com/playlist?list=PLdJpJI2SrqEnaoKgxUlojVDtGE4OyNO0b 에서 확인

--- test/realgrid/src/basic-codes.ts
+++ test/realgrid/src/basic-codes.ts
@@ -1,52 +1,123 @@
-export let basicCode = 
-`<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<link href="/lib/realgrid-style.css" rel="stylesheet" />
-		<script type="text/javascript" src="/lib/realgrid-lic.js"></script>
-		<script type="text/javascript" src="/lib/realgrid.2.3.3.min.js"></script>
-		<script type="text/javascript" src="/js/jquery-3.4.0.min.js"></script>
-	</head>
-	<body>
-		<div id="realgrid" style="width: 100%; height: 440px;">
-		</div>
-	</body>
-</html>
+import * as vscode from 'vscode';
 
-<script>
-    const provider = new RealGrid.LocalDataProvider();
-    const gridView = new RealGrid.GridView("realgrid");
-    gridView.setDataSource(provider);
+// getCompletionItems
 
-    provider.setFields([
-        { fieldName: "KorName",    dataType: "text"     },
-        { fieldName: "Gender",     dataType: "text"     },
-        { fieldName: "Age",        dataType: "number"   },
-        { fieldName: "Phone",      dataType: "text"     },
-        { fieldName: "ProductId",  dataType: "text"     },
-        { fieldName: "KorCountry", dataType: "text"     },
-        { fieldName: "OrderDate",  dataType: "datetime" },
-        { fieldName: "SaveCost",   dataType: "number"   },
-    ]);
+let property_button = new vscode.CompletionItem("button:");
+property_button.insertText = new vscode.SnippetString('button: "${1|none,action,popup|}"');
 
-    gridView.setColumns([
-        { name: "KorName",    fieldName: "KorName",    width:  "80" },
-        { name: "Gender",     fieldName: "Gender",     width:  "60" },
-        { name: "Age",        fieldName: "Age",        width:  "60" },
-        { name: "Phone",      fieldName: "Phone",      width: "120" },
-        { name: "ProductId",  fieldName: "ProductId",  width: "120" },
-        { name: "KorCountry", fieldName: "KorCountry", width: "100" },
-        { name: "OrderDate",  fieldName: "OrderDate",  width:  "80" },
-        { name: "SaveCost",   fieldName: "SaveCost",   width:  "80" },
-    ]);
+let property_children = new vscode.CompletionItem("children:");
+property_children.insertText = new vscode.SnippetString('children: [\n\t${1}\n]');
 
-    var data_url = 
-		"https://raw.githubusercontent.com/realgrid/" +
-    	"open-tutorial/main/datas/data-001.json";
-    $.getJSON(data_url, function (data) {
-        console.log(data);
-        provider.fillJsonData(data, { fillMode: "set" });
-    });
-</script>
-`;
+let property_direction = new vscode.CompletionItem("direction:");
+property_direction.insertText = new vscode.SnippetString('direction: "${1|horizontal,vertical|}"');
+
+let menu_type = new vscode.CompletionItem("type: (menu item)");
+menu_type.insertText = new vscode.SnippetString('type: "${1|check,radio|}"]');
+
+let menu_group = new vscode.CompletionItem("group: (menu item)");
+menu_group.insertText = new vscode.SnippetString('group: "${1}"]');
+
+function keywords() {
+    var result:vscode.CompletionItem[] = [];
+
+    // TODO: gridView와 같은 변수명 등은 현재 파일의 텍스트를 파싱으로 처리하기
+    const words = ["RealGrid", "gridView", "index", "column"];
+
+    for (let word of words) {
+        let item = new vscode.CompletionItem(word);
+        result.push(item);
+    }
+    return result;
+}
+
+function string_properties() {
+    var result:vscode.CompletionItem[] = [];
+    const properties = ["fieldName:", "name:", "label:", "group:", "tag:"];
+    for (let property of properties) {
+        let item = new vscode.CompletionItem(property);
+        item.insertText = new vscode.SnippetString(property + ' "${1}"');
+        result.push(item);
+    }
+    return result;
+}
+
+function number_properties() {
+    var result:vscode.CompletionItem[] = [];
+    const properties = ["step:", "min:", "max:", "delay:"];
+    for (let property of properties) {
+        let item = new vscode.CompletionItem(property);
+        item.insertText = new vscode.SnippetString(property + ' ${1}');
+        result.push(item);
+    }
+    return result;
+}
+
+function boolean_properties() {
+    var result:vscode.CompletionItem[] = [];
+    const properties = ["enabled:", "checked:", "showStepButton:"];
+    for (let property of properties) {
+        let item = new vscode.CompletionItem(property);
+        item.insertText = new vscode.SnippetString(property + ' ${1|true,false|}');
+        result.push(item);
+    }
+    return result;
+}
+
+export function getCompletionItems() {
+    var result:vscode.CompletionItem[] = [];
+    result = result.concat(keywords());
+
+    result = result.concat(string_properties());
+    result = result.concat(boolean_properties());
+
+    result.push(property_button);
+    result.push(property_children);
+    result.push(property_direction);
+
+    result.push(menu_type);
+    result.push(menu_group);
+
+    return result;
+}
+
+// getGridViewCompletionItem
+
+let gridView_columnByName = new vscode.CompletionItem('columnByName');
+gridView_columnByName.insertText = new vscode.SnippetString('columnByName("${1}")');
+
+let gridView_addPopupMenu = new vscode.CompletionItem("addPopupMenu");
+gridView_addPopupMenu.insertText = new vscode.SnippetString('addPopupMenu("${1}", ${2});');
+
+let gridView_setColumnProperty = new vscode.CompletionItem("setColumnProperty");
+gridView_setColumnProperty.insertText = new vscode.SnippetString('setColumnProperty("${1}", "${2}", { ${3} });');
+
+let gridView_onCellButtonClicked = new vscode.CompletionItem("onCellButtonClicked"); 
+gridView_onCellButtonClicked.insertText = new vscode.SnippetString(
+`onCellButtonClickedgrd = function(grid, index, column) {\n
+}`);
+
+let gridView_onMenuItemClicked = new vscode.CompletionItem("onMenuItemClicked"); 
+gridView_onMenuItemClicked.insertText = new vscode.SnippetString(
+`onMenuItemClicked = function(grid, item, clickData) {\n
+}`);
+
+export function getGridViewCompletionItem() {
+    var result:vscode.CompletionItem[] = [];
+    result.push(gridView_columnByName);
+    result.push(gridView_addPopupMenu);
+    result.push(gridView_setColumnProperty);
+    result.push(gridView_onCellButtonClicked);
+    result.push(gridView_onMenuItemClicked);
+    return result;
+}
+
+// getDotCompletionItem
+
+let column_buttonVisibility = new vscode.CompletionItem("buttonVisibility");
+column_buttonVisibility.insertText = new vscode.SnippetString('buttonVisibility = ${2|always,default,visible,hidden|};');
+
+export function getDotCompletionItem() {
+    var result:vscode.CompletionItem[] = [];
+    result.push(column_buttonVisibility);
+    return result;
+}
--- test/realgrid/src/extension.ts
+++ test/realgrid/src/extension.ts
@@ -1,19 +1,11 @@
+import { basename } from 'path';
 import * as vscode from 'vscode';
 import * as bc from './basic-codes';
 
 export function activate(context: vscode.ExtensionContext) {
 	const ruleBasicCode = {
 		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
-			const realGridCompletion = new vscode.CompletionItem('RealGrid');
-
-			const snippetCompletion = new vscode.CompletionItem('RG Basic Code');
-			snippetCompletion.insertText = new vscode.SnippetString(bc.basicCode);
-			snippetCompletion.documentation = new vscode.MarkdownString('Inserts a basic codes for RealGrid.');
-
-			return [
-				realGridCompletion,
-				snippetCompletion,
-			];
+			return bc.getCompletionItems();
 		}		
 	};
 	const providerBasicCode = vscode.languages.registerCompletionItemProvider(
@@ -24,16 +16,13 @@ export function activate(context: vscode.ExtensionContext) {
 	const ruleCompletion = {
 		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
 			const linePrefix = document.lineAt(position).text.substr(0, position.character);
-			if (!linePrefix.endsWith('RealGrid.')) {
-				return undefined;
-			}
 
-			return [
-				new vscode.CompletionItem('LocalDataProvider', vscode.CompletionItemKind.Method),
-				new vscode.CompletionItem('LocalTreeDataProvider', vscode.CompletionItemKind.Method),
-				new vscode.CompletionItem('GridView', vscode.CompletionItemKind.Method),
-				new vscode.CompletionItem('TreeView', vscode.CompletionItemKind.Method),				
-			];
+			if (linePrefix.endsWith('gridView.')) return bc.getGridViewCompletionItem();
+			if (linePrefix.endsWith('index.')) return [new vscode.CompletionItem('itemIndex')];
+			if (linePrefix.endsWith('column.'))return [new vscode.CompletionItem('fieldName')];
+			if (linePrefix.endsWith('.')) return bc.getDotCompletionItem();
+
+			return undefined;
 		}
 	}; 
 	const providerCompletion = vscode.languages.registerCompletionItemProvider(
